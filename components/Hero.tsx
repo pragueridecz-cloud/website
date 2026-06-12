e4eeb1d43679101cce2d14651c7b6919fab75b3f
pragueridecz-cloud <pragueridecz@gmail.com>
2026-06-12 17:00:35 +0200

    fix: duplicitní backgroundSize

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -76,9 +76,8 @@ export default function Hero() {
       <div style={{
         position:"absolute", inset:0, zIndex:0,
         backgroundImage:"url('/hero-prague.png')",
-        backgroundSize:"cover",
-        backgroundPosition:"35% 40%",
         backgroundSize:"130%",
+        backgroundPosition:"35% 40%",
         opacity:0.65,
         mixBlendMode:"luminosity",
       }} />
